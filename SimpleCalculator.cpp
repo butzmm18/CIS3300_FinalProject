9b6d484b0e63f615c91624a4460d51e66ea774e1
butzmm18 <butzmm18@mounties.mansfield.edu>
2016-05-01 20:11:37 -0400

    Create SimpleCalculator.cpp

---
+++ SimpleCalculator.cpp
@@ -0,0 +1,19 @@
+// SimpleCalculator.cpp : Defines the entry point for the console application.
+//
+
+/* SimpleCalculator.cpp by Michaela Butz
+* Declares a variable, assigns the variable, and then calculates the expression.
+**/
+
+#include "stdafx.h"
+#include "iostream"
+
+
+int main()
+{
+	int a;
+	a = 10;
+	std::cout << a + 5 << std::endl;
+
+    return 0;
+}
